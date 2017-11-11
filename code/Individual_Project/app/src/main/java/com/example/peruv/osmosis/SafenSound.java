3c63c31282fcfb3a174530107da6c63d7d8a61ff
sancher6 <sancher6@tcnj.edu>
2017-11-10 20:59:38 -0500

    Full project

---
+++ code/Individual_Project/app/src/main/java/com/example/peruv/osmosis/SafenSound.java
@@ -0,0 +1,13 @@
+package com.example.peruv.osmosis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SafenSound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safen_sound);
+    }
+}
---
+++ code/Individual_Project/app/src/main/java/com/example/peruv/osmosis/WaterTracker.java
@@ -0,0 +1,13 @@
+package com.example.peruv.osmosis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaterTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_tracker);
+    }
+}
